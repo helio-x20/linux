ed4c6cd01598d70c6a46abd3d8a3eeb2a72d1660
Shangbing Hu <shangbing.hu@mediatek.com>
2016-07-13 06:35:02 +0800

    [ALPS02721118] cap touch: add GPL license info
    [Solution]
    add license information to file header of touch driver
    
    MTK-Commit-Id: e66b06cdc48ed91ce593fa4106f049dee52a5cc1
    
    Change-Id: Id020dbee7263f9b1ff4e6badcfffe83204ba83a8
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02721118
    Feature: [Android Default] CapTouch

--- drivers/misc/mediatek/magnetometer/st480/st480.h
+++ drivers/misc/mediatek/magnetometer/st480/st480.h
@@ -1,6 +1,17 @@
 /*
- * Definitions for senodia compass chip.
+ * Copyright (C) 2012 Senodia Corporation.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
  */
+
 #ifndef MAGNETIC_H
 #define MAGNETIC_H
 
